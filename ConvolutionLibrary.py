73355a24d38a4b1699e286feca5f47682fdfafef
ben-polzin <benjaminpolzin@mail.weber.edu>
2024-11-12 16:19:49 -0700

    Created convolution library and cleaned up the deconvolution program.

---
+++ ConvolutionLibrary.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy.fft import fft, ifft
+
+def normalize(audio):
+    return audio / np.max(np.abs(audio))
+
+
+def toStereo(audio):
+    audio = np.vstack([audio, audio])
+    return np.transpose(audio)
+
+
+def lengthMatch(array1, array2):
+    if len(array1) > len(array2):
+        array2_pad = np.zeros((len(array1), 2))
+        array2_pad[:len(array2)] = array2
+        array2 = array2_pad
+
+    if len(array2) > len(array1):
+        array1_pad = np.zeros((len(array2), 2))
+        array1_pad[:len(array1)] = array1
+        array1 = array1_pad
+
+    return array1, array2
+
+
+def deconvolve(wetAudio, dryAudio):
+    wetAudio = normalize(wetAudio)
+    dryAudio = normalize(dryAudio)
+
+    # Convert Mono to Stereo
+    if wetAudio.ndim == 1:
+        wetAudio = toStereo(wetAudio)
+
+    if dryAudio.ndim == 1:
+        dryAudio = toStereo(dryAudio)
+
+    # Match Lengths of Arrays by Padding with Zeros
+    wetAudio, dryAudio = lengthMatch(wetAudio, dryAudio)
+
+    # DeConvolve Audio
+    wetAudio_left, wetAudio_right = np.split(wetAudio, 2, axis=1)
+    dryAudio_left, dryAudio_right = np.split(dryAudio, 2, axis=1)
+
+    wetAudio_left = np.transpose(wetAudio_left)[0]
+    wetAudio_right = np.transpose(wetAudio_right)[0]
+    dryAudio_left = np.transpose(dryAudio_left)[0]
+    dryAudio_right = np.transpose(dryAudio_right)[0]
+
+    deconvolvedAudio_left = ifft(fft(wetAudio_left) / fft(dryAudio_left)).real
+    deconvolvedAudio_right = ifft(fft(wetAudio_right) / fft(dryAudio_right)).real
+
+    deconvolvedAudio_left = normalize(deconvolvedAudio_left)
+    deconvolvedAudio_right = normalize(deconvolvedAudio_right)
+
+    deconvolvedAudio = np.vstack([deconvolvedAudio_left, deconvolvedAudio_right])
+    return np.transpose(deconvolvedAudio)
--- Sweep_Processor.py
+++ Sweep_Processor.py
@@ -4,54 +4,15 @@
 # and Outputs the Impulse Response
 ###################################################
 
-import numpy as np
-import scipy
+from ConvolutionLibrary import deconvolve
+from scipy.io import wavfile
 
 # Read in Files
-samplerateRS, response = scipy.io.wavfile.read('Test Files/Sweeps/SweepSophia.wav')
-samplerateSweep, sweep = scipy.io.wavfile.read('Test Files/Sweeps/Sweep.wav')
+samplerateRS, response = wavfile.read('Test Files/Sweeps/SweepSophia.wav')
+samplerateSweep, sweep = wavfile.read('Test Files/Sweeps/Sweep.wav')
 
-# Normalize Audio
-response = response / np.max(np.abs(response))
-sweep = sweep / np.max(np.abs(sweep))
-
-# Convert Mono to Stereo
-if response.ndim == 1:
-    response = np.vstack([response, response])
-    response = np.transpose(response)
-
-if sweep.ndim == 1:
-    sweep = np.vstack([sweep, sweep])
-    sweep = np.transpose(sweep)
-
-# Match Lengths of Arrays by Padding with Zeros
-if len(response) > len(sweep):
-    sweep_pad = np.zeros((len(response), 2))
-    sweep_pad[:len(sweep)] = sweep
-    sweep = sweep_pad
-
-if len(sweep) > len(response):
-    response_pad = np.zeros((len(sweep), 2))
-    response_pad[:len(response)] = response
-    response = response_pad
-
-# DeConvolve Audio
-response_left, response_right = np.split(response, 2, axis=1)
-sweep_left, sweep_right = np.split(sweep, 2, axis=1)
-
-response_left = np.transpose(response_left)[0]
-response_right = np.transpose(response_right)[0]
-sweep_left = np.transpose(sweep_left)[0]
-sweep_right = np.transpose(sweep_right)[0]
-
-IR_left = scipy.fft.ifft( scipy.fft.fft(response_left) / scipy.fft.fft(sweep_left) ).real
-IR_left = IR_left / np.max(np.abs(IR_left))
-
-IR_right = scipy.fft.ifft( scipy.fft.fft(response_right) / scipy.fft.fft(sweep_right) ).real
-IR_right = IR_right / np.max(np.abs(IR_right))
-
-IR = np.vstack([IR_left, IR_right])
-IR = np.transpose(IR)
+# Extract Impulse Response
+IR = deconvolve(response, sweep)
 
 # Output IR to a Wav File
-scipy.io.wavfile.write("RecoveredImpulseResponse.wav", samplerateRS, IR)
+wavfile.write("RecoveredImpulseResponse.wav", samplerateRS, IR)
